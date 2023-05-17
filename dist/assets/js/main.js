1fe96a95428bb6563e9cc883edad4cb8af135214
Daniil <eo18622@yandex.ru>
2023-05-17 11:34:50 +0300

    страница предложения

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -95,33 +95,37 @@ const swiper1 = new Swiper('#swiper-1', {
 
 });
 
-// Слайдер с пагинацией снизу
-const swiper2 = new Swiper('#swiper-2', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 20,
-
-  // Буллеты
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-
-  breakpoints: {
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 35,
-    },
-
-    1440: {
-      slidesPerView: 4,
+// Слайдер для объектов недвижимости
+document.addEventListener('DOMContentLoaded', () => {
+  const width = window.innerWidth
+  if (width < 1440) {
+    const swiper2 = new Swiper('#swiper-2', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
       spaceBetween: 20,
-    }
-  }
 
+      // Буллеты
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+
+      breakpoints: {
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 35,
+        },
+
+        1440: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+        }
+      }
+    });
+  }
 });
 
 
@@ -151,6 +155,20 @@ const swiper3 = new Swiper('#swiper-3', {
 
 });
 
+
+
+const swiper4 = new Swiper('#swiper-4', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Буллеты
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
 
 // effect: 'coverflow',
 
