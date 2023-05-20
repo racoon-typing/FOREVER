71a8e8ec4c25ebe0a214d8346f78499657e61956
Daniil <eo18622@yandex.ru>
2023-05-20 17:05:39 +0300

    Главная страница готова для рекламы

---
+++ src/assets/js/components/scroll.js
@@ -0,0 +1,7 @@
+<!-- Якорная ссылка -->
+    <script>
+        function scrollToStep1() {
+            var step1Element = document.getElementById("step1");
+            step1Element.scrollIntoView({ behavior: "smooth" });
+        }
+    </script>
